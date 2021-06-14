7e689f0aaa2fedc54d1202e15d4ec3baa0b0f47e
wellhausen <3402403+wellhausen@users.noreply.github.com>
2021-06-14 14:27:01 -0500

    utilize description

--- app/components/PortfolioSection/index.js
+++ app/components/PortfolioSection/index.js
@@ -87,11 +87,11 @@ const PortfolioSection = ({
                       <div className="decription">
                         <h5>{item[0].otherInfo.projectName}</h5>
                         <div className="sub-heading">
-                          {item[0].otherInfo.sqrft} sq ft
+                          {item[0].otherInfo.description}
                         </div>
                         <div className="sub-heading">
-                          {item[0].otherInfo.bedroom} bedroom |{' '}
-                          {item[0].otherInfo.bathrooms} bath
+                          {/* {item[0].otherInfo.bedroom} bedroom |{' '}
+                          {item[0].otherInfo.bathrooms} bath */}
                         </div>
                         <p>{item[0].otherInfo.shortDescription}</p>
                       </div>
